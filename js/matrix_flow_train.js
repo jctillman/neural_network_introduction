3ef7e3ce7a91a13f542ba6cbee96ffac9b3dc28a
James Tillman <james.tillman.7@gmail.com>
2016-11-25 17:24:21 -0500

    Adding first version of mgd.

--- js/matrix_flow_train.js
+++ js/matrix_flow_train.js
@@ -12,19 +12,51 @@ class AbstractOptimizer {
 class GradientDescent extends AbstractOptimizer {
 
 	constructor(lr){
-		super()
+		super();
 		this.lr = lr
 	}
 
 	run(model, minId, givenIds, givenVals){
-
-		var res = model.run([minId],givenIds,givenVals)
-		//console.log(minId)
-		var gradient = model.getAllParamGradients(minId)
+		var res = model.run([minId],givenIds,givenVals);
+		var gradient = model.getAllParamGradients(minId);
 		var changedGradients = util.objMap(gradient, (op, key) => {
-			return op.piecewise( x => -x * this.lr)
+			return op.piecewise( x => -x * this.lr);
 		});
 		return model.newAltered(changedGradients)
+	}
+
+}
+
+
+class MomentGradientDescent extends AbstractOptimizer {
+
+	constructor(lr, momentum_decay){
+		super();
+		this.momentum_decay = momentum_decay;
+		this.lr = lr;
+		this.direction = undefined;
+	}
+
+	run(model, minId, givenIds, givenVals){
+		var res = model.run([minId],givenIds,givenVals);
+		var gradient = model.getAllParamGradients(minId);
+		var changedGradients = util.objMap(gradient, (op, key) => {
+			return op.piecewise( x => -x * this.lr);
+		});
+
+		if (this.direction == undefined){
+			this.direction = util.objMap(gradient, (op) => op.piecewise( () => 0));
+		}else{
+			this.direction = util.objMap(this.direction, (op, key) => {
+				return op.piecewise( x => x * this.momentum_decay);
+			});
+		}
+
+		this.direction = obj.map(changedGradients, (op, key) => {
+			return changedGradients[key].add(this.direction[key]);
+		})
+
+		return model.newAltered(this.direction);
 	}
 
 }

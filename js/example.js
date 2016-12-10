c0429bbd38788fbd08865bc9ed5c911305af0137
James Tillman <james.tillman.7@gmail.com>
2016-12-10 11:03:27 -0500

    Starting autoencoder work.

--- js/example.js
+++ js/example.js
@@ -1,1 +1,3 @@
 require('./example_01ann_fc.js');
+
+require('./example_02ann_auto.js');
--- js/example_02ann_auto.js
+++ js/example_02ann_auto.js
@@ -5,28 +5,32 @@ const Matrix = require('./line_alge.js').Matrix
 
 describe("Can train a basic fully connected sigmoid network", function(){
 
-	xit('Can train a basic fc sigmoid classifier', function(){
+	//Doesn't work at the moment.
+	xit('Can train a basic fc sigmoid autoencoder', function(){
 
 		const [trainer, tester] = mnist.batchMakers(0.9);
-		const batchSize = 25;
+		const batchSize = 10;
 
 		const tr = new mf.train.GradientDescent(0.01);
 		var mdl = new mf.Model();
 		const o = mf.ops.util.ObjOpWrapper(mf.ops.lib, mdl);
 
 		const input = o.Given()
-		const output = o.Given()
 		const w1 = o.Param(Matrix.make([784,100],() => (Math.random()-0.5)/100.00 ))
 		const l1 = o.Sigmoid(o.Mult(input, w1))
-		const w2 = o.Param(Matrix.make([100,10],() => (Math.random()-0.5)/100.00 ))
+		const w2 = o.Param(Matrix.make([100,20],() => (Math.random()-0.5)/100.00 ))
 		const l2 = o.Sigmoid(o.Mult(l1, w2))
+		const w3 = o.Param(Matrix.make([20,100],() => (Math.random()-0.5)/100.00 ))
+		const l3 = o.Sigmoid(o.Mult(l2, w3))
+		const w4 = o.Param(Matrix.make([100,784],() => (Math.random()-0.5)/100.00 ))
+		const l4 = o.Sigmoid(o.Mult(l3, w4))
 
-		const loss = o.ReduceSum(o.Pow(o.Sub(output,l2),2))
+		const loss = o.ReduceSum(o.Pow(o.Sub(input,l4),2))
 
 		for(var i = 0; i < 20; i++){
 
 			var [matIn, matOut] = trainer(batchSize);
-			var train = {[input]: matIn, [output]: matOut }
+			var train = {[input]: matIn}
 
 			mdl = tr.run(mdl, loss, train);
 
